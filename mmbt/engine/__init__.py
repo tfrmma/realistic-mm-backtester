33ca32e328486c75324251a30890e2733aa0d432
Taha MM <fadilrezokt@gmail.com>
2026-07-15 20:56:14 +0100

    Import MultiAssetEngine and enhance sweep imports
    Added MultiAssetEngine and additional sweep functions to __init__.py.

--- mmbt/engine/__init__.py
+++ mmbt/engine/__init__.py
@@ -1,10 +1,22 @@
 from mmbt.engine.simple import BacktestEngine
 from mmbt.engine.pro import ProBacktestEngine
-from mmbt.engine.sweep import ParameterSweep, SweepResult, expand_grid
+from mmbt.engine.multi_asset import MultiAssetEngine
+from mmbt.engine.sweep import (
+    OutOfSampleResult,
+    ParameterSweep,
+    SweepResult,
+    WalkForwardFold,
+    expand_grid,
+    out_of_sample_validate,
+    walk_forward_summary,
+    walk_forward_validate,
+)
 from mmbt.reporting.metrics import BacktestReport, StrategyMetrics
 
 __all__ = [
-    "BacktestEngine", "ProBacktestEngine",
+    "BacktestEngine", "ProBacktestEngine", "MultiAssetEngine",
     "ParameterSweep", "SweepResult", "expand_grid",
+    "OutOfSampleResult", "WalkForwardFold",
+    "out_of_sample_validate", "walk_forward_validate", "walk_forward_summary",
     "BacktestReport", "StrategyMetrics",
 ]
