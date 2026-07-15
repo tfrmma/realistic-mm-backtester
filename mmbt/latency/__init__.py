29306eed9771df310d425c5390b6866130e3c3ab
Taha MM <fadilrezokt@gmail.com>
2026-07-15 20:54:57 +0100

    Add BookHistory to __all__ in __init__.py

--- mmbt/latency/__init__.py
+++ mmbt/latency/__init__.py
@@ -1,4 +1,5 @@
 from mmbt.latency.config import LatencyConfig
 from mmbt.latency.simulator import LatencySimulator
+from mmbt.latency.book_history import BookHistory
 
-__all__ = ["LatencyConfig", "LatencySimulator"]
+__all__ = ["LatencyConfig", "LatencySimulator", "BookHistory"]
